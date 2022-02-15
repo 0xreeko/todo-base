5ed585abdf815506793639ba5ec07cabf830892c
0xreeko <enric_trillo@outlook.com>
2022-02-15 10:53:46 +0000

    finished zustand example

--- src/store.ts
+++ src/store.ts
@@ -38,4 +38,52 @@ type TodoStore = {
   newTodo: string;
   addTodo: () => void;
   setNewTodo: (newTodo: string) => void;
+  update: (id: number, text: string) => void;
+  toggle: (id: number) => void;
+  remove: (id: number) => void;
+  load: (todos: Todo[]) => void;
 };
+
+// initialising the store
+const useStore = create<TodoStore>((set) => ({
+  todos: [],
+  newTodo: "",
+  load: (todos: Todo[]) => {
+    set((state) => ({
+      ...state,
+      todos,
+    }));
+  },
+  addTodo: () => {
+    set((state) => ({
+      todos: addTodo(state.todos, state.newTodo),
+      newTodo: "",
+    }));
+  },
+  setNewTodo: (newTodo: string) => {
+    set((state) => ({
+      ...state,
+      newTodo,
+    }));
+  },
+  update: (id: number, text: string) => {
+    set((state) => ({
+      ...state,
+      todos: updateTodo(state.todos, id, text),
+    }));
+  },
+  toggle: (id: number) => {
+    set((state) => ({
+      ...state,
+      todos: toggleTodo(state.todos, id),
+    }));
+  },
+  remove: (id: number) => {
+    set((state) => ({
+      ...state,
+      todos: removeTodo(state.todos, id),
+    }));
+  },
+}));
+
+export default useStore;
